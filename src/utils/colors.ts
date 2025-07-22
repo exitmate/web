686cf4822e648bd5ff2de326b749ca1ebf8e6d84
mingkyeongg <ming0820@naver.com>
2025-07-22 16:23:41 +0900

    feat: 색상 팔레트 설정

---
+++ src/utils/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  point : "#3f8ae3",
+  white: "#ffffff",
+  gray: {
+    1 : "#f9fbfd",
+    2 : "#f3f4f6",
+    3 : "#e6e8ec",
+    4 : "#caced4",
+    5 : "#bdc1c7",
+    6 : "#a9afb8",
+    7 : "#595e69",
+    8 : "#343537",
+  },
+  black: "#1b1b1b",
+}
+
+export default colors;
