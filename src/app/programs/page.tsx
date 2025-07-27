2da27cd4431e6750cbf32a7698d0c5c315ff7499
mingkyeongg <ming0820@naver.com>
2025-07-27 17:28:01 +0900

    refactor: Card 컴포넌트 분리

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import MyIcon from "@/assets/icons/my.svg";
-import Card from "@/components/common/Card";
 import Category from "@/components/common/Category";
 import PageNation from "@/components/common/PageNation";
 import Toggle from "@/components/common/Toggle";
 import ProgramCard from "@/components/ProgramCard";
+import UserCard from "@/components/UserCard";
 import { useState } from "react";
 
 export const ProgramsPage = () => {
@@ -13,7 +13,7 @@ export const ProgramsPage = () => {
   const [isOn, setIsOn] = useState(false);
   const [category, setCategory] = useState("마감 유형");
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 20; // 예시로 20페이지 설정
+  const totalPages = 20;
 
   return (
     <div>
@@ -24,7 +24,11 @@ export const ProgramsPage = () => {
         deadline="2023-12-31"
         centerName="부산시소상공인종합지원센터"
       />
-      <Card programCount={10} title={["김이름님이 신청가능한", "지원사업 개수"]} iconUrl={MyIcon} />
+      <UserCard
+        imageUrl={MyIcon}
+        title={["김이름님이 신청가능한", "지원사업 개수"]}
+        programCount={10}
+      />
       <Toggle isOn={isOn} setIsOn={setIsOn} />
       <Category category={category} contents={["상시모집", "마감일 존재"]} setCategory={setCategory} />
       <PageNation 
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import colors from "@/utils/colors";
+import styled from "@emotion/styled";
+import Image from "next/image";
+import Card from "./common/Card";
+
+interface UserCardProps {
+  imageUrl: string;
+  title: string[];
+  programCount: number;
+}
+
+export const UserCard = ({ imageUrl, title, programCount }: UserCardProps) => {
+  return (
+    <Card>
+      <Image src={imageUrl} alt="user" width={24} height={24} />
+      <TitleContainer>
+        {title.map((line, index) => (
+          <TitleText key={index}>{line}</TitleText>
+        ))}
+      </TitleContainer>
+      <ProgramCountText>{programCount}</ProgramCountText>
+    </Card>
+  );
+};
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  margin-top: 8px;
+`;
+
+const TitleText = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  color: ${colors.black};
+  margin: 0;
+`;
+
+const ProgramCountText = styled.p`
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
+  font-size: 36px;
+  font-weight: 700;
+  color: ${colors.point};
+`;
+
+export default UserCard;
