4a187102cfd38a6d9cc356ddc0d64f0136137021
cla6shade <cla6shade@gmail.com>
2025-08-10 23:36:54 +0900

    test: info input test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import ClientLayout from '@/components/ClientLayout'
 import AuthTest from '@/features/auth/AuthTest'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/authOptions'
+import InfoInputTest from '@/features/auth/InfoInputTest'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
   return (
     <ClientLayout session={session}>
       <AuthTest />
+      <InfoInputTest />
     </ClientLayout>
   )
 }
