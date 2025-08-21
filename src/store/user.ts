fd4671b3a409994a4ebf1883fce169b6d36aa837
mingkyeongg <ming0820@naver.com>
2025-08-21 23:50:23 +0900

    feat: 사용자 스토어에 비즈니스 정보 추가 및 관련 메서드 구현

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,8 +1,10 @@
-import { Gender, Member } from '@/generated/prisma'
+import { BusinessInfo, Gender, Member } from '@/generated/prisma'
 import { create } from 'zustand'
 
 type UserStore = Member & {
+  businessInfo: BusinessInfo | null
   setUser: (user: Partial<Member>) => void
+  setBusinessInfo: (info: Partial<BusinessInfo>) => void
   resetUser: () => void
 }
 
@@ -18,15 +20,29 @@ const initialUser: Member = {
   agreedTermsOfUse: false,
   agreedDataUsage: false,
   agreedMarketing: false,
-  businessInfo: null,
   createdAt: new Date(),
   updatedAt: new Date(),
 }
 
 const useUserStore = create<UserStore>((set) => ({
   ...initialUser,
-  setUser: (user) => set((state) => ({ ...state, ...user })),
-  resetUser: () => set(initialUser),
+  businessInfo: null,
+  setUser: (user) => set((state) => ({
+    ...state,
+    ...user,
+  })),
+  resetUser: () => set((state) => ({
+    ...state,
+    ...initialUser,
+    businessInfo: null,
+  })),
+  setBusinessInfo: (info) =>
+    set((state) => ({
+      ...state,
+      businessInfo: state.businessInfo
+        ? { ...state.businessInfo, ...info }
+        : ({ ...info } as BusinessInfo),
+    })),
 }))
 
 export default useUserStore
