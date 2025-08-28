2da80446c78411b64618571c88ea5748ec34d58d
cla6shade <cla6shade@github.com>
2025-08-28 23:16:26 +0900

    feat: 에러 응답 작성 함수 구현

--- src/app/api/utils.ts
+++ src/app/api/utils.ts
@@ -1,4 +1,5 @@
-import { Pagination } from './schema'
+import z from 'zod'
+import { ErrorResponse, Pagination, ValidationErrorResponse } from './schema'
 
 export function getPaginatedParams(
   page: number,
@@ -27,4 +28,21 @@ export function getPaginationInfo(
     hasNextPage: currentPage < totalPages,
     hasPrevPage: currentPage > 1,
   }
+}
+
+export function buildErrorResponse<T extends Error>(
+  error: T,
+  errorMessage?: T extends z.ZodError ? undefined : string,
+): T extends z.ZodError ? ValidationErrorResponse : ErrorResponse {
+  if (error instanceof z.ZodError) {
+    return {
+      errors: error.issues.map((issue) => ({
+        field: issue.path.join('.'),
+        message: issue.message,
+      })),
+    } as any
+  }
+  return {
+    error: errorMessage,
+  } as any
 }
