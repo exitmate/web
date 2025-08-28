3d8b948149c98ab03b4e5a99bd8f85bca5dc427c
cla6shade <cla6shade@github.com>
2025-08-28 23:21:07 +0900

    feat: 사업 지원가능 여부 반환 메소드 작성

---
+++ src/app/api/projects/count/route.ts
@@ -0,0 +1,30 @@
+import prisma from '@/utils/prisma'
+import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
+import { getPersonalizedWhere } from '../search'
+import { GetAppliableProjectCountResponse } from './schema'
+
+export async function GET(request: NextRequest) {
+  const token = await getToken({ req: request })
+  if (!token) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+
+  const businessInfo = await prisma.businessInfo.findFirst({
+    where: {
+      memberId: token.id,
+    },
+  })
+  if (!businessInfo) {
+    return NextResponse.json(
+      { error: 'BusinessInfo not found' },
+      { status: 404 },
+    )
+  }
+
+  const where = getPersonalizedWhere(businessInfo)
+  const count = await prisma.supportProject.count({ where })
+  return NextResponse.json<GetAppliableProjectCountResponse>({
+    data: { count },
+  })
+}
