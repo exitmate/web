9c5162f242ac1a5b633818968f7c4697916e7fe1
mingkyeongg <ming0820@naver.com>
2025-08-30 21:26:07 +0900

    feat: 프로젝트 상세 레이아웃에서 메타데이터 제거

--- src/app/projects/[id]/layout.tsx
+++ src/app/projects/[id]/layout.tsx
@@ -2,11 +2,6 @@ import { ProjectDetailProvider } from "@/features/projects/detail/ProjectDetailC
 import prisma from "@/utils/prisma"
 import { redirect } from "next/navigation"
 
-// app/project/layout.tsx
-export const metadata = {
-  title: 'Projects',
-}
-
 export default async function ProjectDetailLayout({
   children,
   params,
